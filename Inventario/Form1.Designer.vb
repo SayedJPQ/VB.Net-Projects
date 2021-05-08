50b32e752742edeb57aad5fb474910381edb8e30
SayedJPQ <sayedjquiros@gmail.com>
2021-05-08 16:54:05 -0600

    Inventario

---
+++ Inventario/Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim CodigoLabel As System.Windows.Forms.Label
+        Dim ProductoLabel As System.Windows.Forms.Label
+        Dim PrecioLabel As System.Windows.Forms.Label
+        Dim StockLabel As System.Windows.Forms.Label
+        Me.InventarioDataSet = New Inventario.InventarioDataSet()
+        Me.InventarioBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.InventarioTableAdapter = New Inventario.InventarioDataSetTableAdapters.InventarioTableAdapter()
+        Me.TableAdapterManager = New Inventario.InventarioDataSetTableAdapters.TableAdapterManager()
+        Me.InventarioBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.InventarioBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.CodigoTextBox = New System.Windows.Forms.TextBox()
+        Me.ProductoTextBox = New System.Windows.Forms.TextBox()
+        Me.PrecioTextBox = New System.Windows.Forms.TextBox()
+        Me.StockTextBox = New System.Windows.Forms.TextBox()
+        CodigoLabel = New System.Windows.Forms.Label()
+        ProductoLabel = New System.Windows.Forms.Label()
+        PrecioLabel = New System.Windows.Forms.Label()
+        StockLabel = New System.Windows.Forms.Label()
+        CType(Me.InventarioDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.InventarioBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.InventarioBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.InventarioBindingNavigator.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'InventarioDataSet
+        '
+        Me.InventarioDataSet.DataSetName = "InventarioDataSet"
+        Me.InventarioDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'InventarioBindingSource
+        '
+        Me.InventarioBindingSource.DataMember = "Inventario"
+        Me.InventarioBindingSource.DataSource = Me.InventarioDataSet
+        '
+        'InventarioTableAdapter
+        '
+        Me.InventarioTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.InventarioTableAdapter = Me.InventarioTableAdapter
+        Me.TableAdapterManager.UpdateOrder = Inventario.InventarioDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'InventarioBindingNavigator
+        '
+        Me.InventarioBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.InventarioBindingNavigator.BindingSource = Me.InventarioBindingSource
+        Me.InventarioBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.InventarioBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.InventarioBindingNavigator.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InventarioBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.InventarioBindingNavigatorSaveItem})
+        Me.InventarioBindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.InventarioBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.InventarioBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.InventarioBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.InventarioBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.InventarioBindingNavigator.Name = "InventarioBindingNavigator"
+        Me.InventarioBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.InventarioBindingNavigator.Size = New System.Drawing.Size(863, 28)
+        Me.InventarioBindingNavigator.TabIndex = 0
+        Me.InventarioBindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 25)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 25)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 28)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 23)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(56, 25)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 28)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 25)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 25)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 28)
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 25)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 25)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'InventarioBindingNavigatorSaveItem
+        '
+        Me.InventarioBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.InventarioBindingNavigatorSaveItem.Image = CType(resources.GetObject("InventarioBindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.InventarioBindingNavigatorSaveItem.Name = "InventarioBindingNavigatorSaveItem"
+        Me.InventarioBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 25)
+        Me.InventarioBindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'CodigoLabel
+        '
+        CodigoLabel.AutoSize = True
+        CodigoLabel.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        CodigoLabel.Location = New System.Drawing.Point(5, 61)
+        CodigoLabel.Name = "CodigoLabel"
+        CodigoLabel.Size = New System.Drawing.Size(70, 21)
+        CodigoLabel.TabIndex = 1
+        CodigoLabel.Text = "Codigo:"
+        '
+        'CodigoTextBox
+        '
+        Me.CodigoTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.InventarioBindingSource, "Codigo", True))
+        Me.CodigoTextBox.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CodigoTextBox.Location = New System.Drawing.Point(109, 53)
+        Me.CodigoTextBox.Name = "CodigoTextBox"
+        Me.CodigoTextBox.Size = New System.Drawing.Size(245, 29)
+        Me.CodigoTextBox.TabIndex = 2
+        '
+        'ProductoLabel
+        '
+        ProductoLabel.AutoSize = True
+        ProductoLabel.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        ProductoLabel.Location = New System.Drawing.Point(5, 96)
+        ProductoLabel.Name = "ProductoLabel"
+        ProductoLabel.Size = New System.Drawing.Size(84, 21)
+        ProductoLabel.TabIndex = 3
+        ProductoLabel.Text = "Producto:"
+        '
+        'ProductoTextBox
+        '
+        Me.ProductoTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.InventarioBindingSource, "Producto", True))
+        Me.ProductoTextBox.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ProductoTextBox.Location = New System.Drawing.Point(109, 88)
+        Me.ProductoTextBox.Name = "ProductoTextBox"
+        Me.ProductoTextBox.Size = New System.Drawing.Size(245, 29)
+        Me.ProductoTextBox.TabIndex = 4
+        '
+        'PrecioLabel
+        '
+        PrecioLabel.AutoSize = True
+        PrecioLabel.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        PrecioLabel.Location = New System.Drawing.Point(5, 133)
+        PrecioLabel.Name = "PrecioLabel"
+        PrecioLabel.Size = New System.Drawing.Size(62, 21)
+        PrecioLabel.TabIndex = 5
+        PrecioLabel.Text = "Precio:"
+        '
+        'PrecioTextBox
+        '
+        Me.PrecioTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.InventarioBindingSource, "Precio", True))
+        Me.PrecioTextBox.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PrecioTextBox.Location = New System.Drawing.Point(109, 125)
+        Me.PrecioTextBox.Name = "PrecioTextBox"
+        Me.PrecioTextBox.Size = New System.Drawing.Size(245, 29)
+        Me.PrecioTextBox.TabIndex = 6
+        '
+        'StockLabel
+        '
+        StockLabel.AutoSize = True
+        StockLabel.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        StockLabel.Location = New System.Drawing.Point(9, 171)
+        StockLabel.Name = "StockLabel"
+        StockLabel.Size = New System.Drawing.Size(58, 21)
+        StockLabel.TabIndex = 7
+        StockLabel.Text = "Stock:"
+        '
+        'StockTextBox
+        '
+        Me.StockTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.InventarioBindingSource, "Stock", True))
+        Me.StockTextBox.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StockTextBox.Location = New System.Drawing.Point(109, 163)
+        Me.StockTextBox.Name = "StockTextBox"
+        Me.StockTextBox.Size = New System.Drawing.Size(245, 29)
+        Me.StockTextBox.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(863, 450)
+        Me.Controls.Add(CodigoLabel)
+        Me.Controls.Add(Me.CodigoTextBox)
+        Me.Controls.Add(ProductoLabel)
+        Me.Controls.Add(Me.ProductoTextBox)
+        Me.Controls.Add(PrecioLabel)
+        Me.Controls.Add(Me.PrecioTextBox)
+        Me.Controls.Add(StockLabel)
+        Me.Controls.Add(Me.StockTextBox)
+        Me.Controls.Add(Me.InventarioBindingNavigator)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.InventarioDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.InventarioBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.InventarioBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.InventarioBindingNavigator.ResumeLayout(False)
+        Me.InventarioBindingNavigator.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents InventarioDataSet As InventarioDataSet
+    Friend WithEvents InventarioBindingSource As BindingSource
+    Friend WithEvents InventarioTableAdapter As InventarioDataSetTableAdapters.InventarioTableAdapter
+    Friend WithEvents TableAdapterManager As InventarioDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents InventarioBindingNavigator As BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents InventarioBindingNavigatorSaveItem As ToolStripButton
+    Friend WithEvents CodigoTextBox As TextBox
+    Friend WithEvents ProductoTextBox As TextBox
+    Friend WithEvents PrecioTextBox As TextBox
+    Friend WithEvents StockTextBox As TextBox
+End Class
---
+++ Inventario/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub InventarioBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles InventarioBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.InventarioBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.InventarioDataSet)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'InventarioDataSet.Inventario' table. You can move, or remove it, as needed.
+        Me.InventarioTableAdapter.Fill(Me.InventarioDataSet.Inventario)
+
+    End Sub
+End Class
